6d0f86dd75c48f6c09a1e209719e085f079a9a67
bdreaves04 <bdreaves04@gmail.com>
2023-10-24 17:18:06 +0100

    added code to attempt to populate array of records from csv file

--- src/main.c
+++ src/main.c
@@ -7,6 +7,19 @@ int main(int argc, char *argv[]) {
   FITNESS_DATA records[512];
   FILE *file = openFile("../static/data.csv", "r");
 
+  char line_buffer[512];
+  int i = 0;
+  while (fgets(line_buffer, sizeof(line_buffer), file)) {
+    tokeniseRecordModified(line_buffer, ",", &records[i]);
+    i++;
+  }
+
+  for (int j = 0; j < i; j++) {
+    printf("Date: %s\n", records[j].date);
+    printf("Time: %s\n", records[j].time);
+    printf("Steps: %s\n", records[j].steps);
+  }
+
   // tokeniseRecordModified("2020-01-01,12:00,1000", ",", &records);
   //
   // printf("Date: %s\n", records.date);
