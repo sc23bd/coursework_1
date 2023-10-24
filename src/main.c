1397da60a6ad693b98c798925eec4370711b3c8f
bdreaves04 <bdreaves04@gmail.com>
2023-10-24 17:34:25 +0100

    adding printing of first three records

--- src/main.c
+++ src/main.c
@@ -21,6 +21,9 @@ int main(int argc, char *argv[]) {
   // }
 
   printf("Number of records in file: %d\n", i);
+  for (int a = 0; a < 3; a++) {
+    printf("%s/%s/%s\n", records[a].date, records[a].time, records[a].steps);
+  }
 
   fclose(file);
   return EXIT_SUCCESS;
